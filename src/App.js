268a85c46c316800e2efe89703b01a8667617435
PuhJan <ferjohn@bpoc.co.jp>
2022-06-08 11:04:44 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Button from './components/Button';
 import { FaHeart } from "react-icons/fa";
+import Label from './components/Label/Label';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button buttonStyle="btn--danger--outline" buttonSize="btn--large" buttonBorder="btn-circle" iconPlace="btn-iconLeft"><FaHeart/> REGISTRATION</Button>
+        <Label sizeFont="lbl-fontExLarge" labelSize="lbl-small" iconPlace="lbl-iconLeft" fontWeight="lbl-regular" fontStyle="lbl-sans-serif" textAlign="lbl-center"> <FaHeart/>fontstyle</Label>
+        <Button buttonStyle="btn--primary--outline" buttonSize="btn--large" buttonBorder="btn-circle" iconPlace="btn-iconLeft"><FaHeart/> CLICK</Button>
       </header>
     </div>
   );
